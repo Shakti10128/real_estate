4b03eb3fdeb6fcbe4bd702bb207b13424000838c
shakti <kumarshakti2017@gmail.com>
2024-03-05 13:41:11 +0530

    install react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  console.log("first")
+  return (
+    <>
+      <div className=''>
+        Hello react with vite
+      </div>
+    </>
+  )
+}
+
+export default App
