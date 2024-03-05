8d0c81a3d10ffd973324d6f0fc2493516a2da0ce
shakti <kumarshakti2017@gmail.com>
2024-03-05 14:01:53 +0530

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from "./pages/Profile"
+import About from './pages/About'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   console.log("first")
   return (
-    <>
-      <div className=''>
-        Hello react with vite
-      </div>
-    </>
+        <BrowserRouter >
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/sign-in" element={<SignIn />}/>
+            <Route path="/sign-up" element={<SignUp/>}/>
+            <Route path="/profile" element={<Profile/>}/>
+            <Route path="/about" element={<About/>}/>
+          </Routes>
+        </BrowserRouter>
   )
 }
 
